7ae33a1f3ab4652dea1b31fbfcbfd63cdf7fbb1f
VladPshinokov <55715341+VladPshinokov@users.noreply.github.com>
2020-09-17 13:14:21 +0300

    Add files via upload

---
+++ FindTextForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextReader
+{
+    public partial class FindTextForm : Form
+    {
+        private static string inputText;
+        private static int numEnter = 0;
+        private static int[] arrIndexStr = new int[50];
+        private static int numWords = 0;
+        
+        public FindTextForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonFind2Form_Click(object sender, EventArgs e)
+        {
+            // Запись введеного для поиска слова в статическую переменную
+            
+            inputText = FindTextBox.Text;
+            this.Close();
+        }
+
+        public static void FindText(in string srcString, ref int indexSubStr, ref int subStrLength)
+        {
+            // Обнуление статических счетчиков при поиске нового слова
+            
+            numEnter = 0;
+            numWords = 0;
+            
+            if((indexSubStr = srcString.IndexOf(inputText)) != (-1))
+            {
+                for (int i = 0; i < indexSubStr; i++)
+                    if (srcString[i].Equals('\n'))
+                        numEnter++;
+
+                indexSubStr -= numEnter; // сдвиг индекса начала найденного слова на число знаков переноса строк
+               
+                arrIndexStr[numWords] = indexSubStr;  // запись итогового индекса в массив для последющего использования в FindPrevWord(),
+                                                      // т.е. можно перемещаться между выделениями в тексте по индескам в массиве
+                subStrLength = inputText.Length;
+            }
+        }
+
+        public static void FindNextWord(in string srcString, ref int indexSubStr)
+        {
+            // Проверка на наличие слова в тексте, если функция поиска FindText() не нашла слово, то indexSubStr == (-1), соответственно
+            // нажатие на кнопку поиска следующего слова (которое приводит в эту функцию) не должно приводить ни к каким действиям, что и реализовано в данном блоке
+            
+            if (indexSubStr != (-1))
+            {
+                int tempIndex = 0;
+
+                tempIndex = indexSubStr; // запоминаем текущее значение индекса, ищем новое, поиск переносов \n выполняем в интервале между ними
+
+                indexSubStr = srcString.IndexOf(inputText, (indexSubStr + inputText.Length));
+
+                if (indexSubStr != (-1)) // если больше слов в тексте нет, то возвращаемся в главную форму, переданная по ссылке indexSubStr установлена в (-1)
+                {
+                    for (int i = tempIndex; i < indexSubStr; i++)
+                        if (srcString[i].Equals('\n'))
+                            numEnter++;
+
+                    indexSubStr -= numEnter;
+
+                    arrIndexStr[++numWords] = indexSubStr;
+                }
+            }
+        }
+
+        public static int FindPrevWord(ref int indexSubStr)
+        {
+            // Двигаем переданный по ссылке индекс назад, чтобы при следующем вызове функции FindNext(), поиск слова (и выделение в тексте) происходил именно с этой позиции,
+            // а не с оставленной при последнем вызове FindNext()
+            
+            indexSubStr = arrIndexStr[--numWords];
+            return arrIndexStr[numWords];
+        }
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TextReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonNextFindWord = new System.Windows.Forms.Button();
+            this.buttonPrevFindWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(65, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(624, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добро пожаловать в приложение TextReader! Загрузите текстовый файл, и наслаждайте" +
+    "сь!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(716, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(893, 53);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(33, 33);
+            this.buttonPlus.TabIndex = 5;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(893, 92);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(33, 33);
+            this.buttonMinus.TabIndex = 6;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonFind.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonFind.Image = ((System.Drawing.Image)(resources.GetObject("buttonFind.Image")));
+            this.buttonFind.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonFind.Location = new System.Drawing.Point(893, 131);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(33, 33);
+            this.buttonFind.TabIndex = 7;
+            this.buttonFind.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.buttonFind.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(874, 454);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonNextFindWord
+            // 
+            this.buttonNextFindWord.Location = new System.Drawing.Point(893, 171);
+            this.buttonNextFindWord.Name = "buttonNextFindWord";
+            this.buttonNextFindWord.Size = new System.Drawing.Size(33, 33);
+            this.buttonNextFindWord.TabIndex = 9;
+            this.buttonNextFindWord.Text = ">>";
+            this.buttonNextFindWord.UseVisualStyleBackColor = true;
+            this.buttonNextFindWord.Click += new System.EventHandler(this.buttonNextFindWord_Click);
+            // 
+            // buttonPrevFindWord
+            // 
+            this.buttonPrevFindWord.Location = new System.Drawing.Point(893, 211);
+            this.buttonPrevFindWord.Name = "buttonPrevFindWord";
+            this.buttonPrevFindWord.Size = new System.Drawing.Size(33, 33);
+            this.buttonPrevFindWord.TabIndex = 10;
+            this.buttonPrevFindWord.Text = "<<";
+            this.buttonPrevFindWord.UseVisualStyleBackColor = true;
+            this.buttonPrevFindWord.Click += new System.EventHandler(this.buttonPrevFindWord_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 519);
+            this.Controls.Add(this.buttonPrevFindWord);
+            this.Controls.Add(this.buttonNextFindWord);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Text Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonNextFindWord;
+        private System.Windows.Forms.Button buttonPrevFindWord;
+    }
+}
+
