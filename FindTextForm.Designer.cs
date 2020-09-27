6fb1d9af92d31718341af31599a32a94dea4f45c
VladPshinokov <55715341+VladPshinokov@users.noreply.github.com>
2020-09-27 17:44:35 +0300

    Add files via upload

---
+++ FindTextForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TextReader
+{
+    partial class FindTextForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.FindTextBox = new System.Windows.Forms.TextBox();
+            this.buttonFind2Form = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите слово (фразу), которую хотите найти в тексте:";
+            // 
+            // FindTextBox
+            // 
+            this.FindTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FindTextBox.Location = new System.Drawing.Point(16, 34);
+            this.FindTextBox.Name = "FindTextBox";
+            this.FindTextBox.Size = new System.Drawing.Size(374, 21);
+            this.FindTextBox.TabIndex = 1;
+            // 
+            // buttonFind2Form
+            // 
+            this.buttonFind2Form.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFind2Form.Location = new System.Drawing.Point(397, 32);
+            this.buttonFind2Form.Name = "buttonFind2Form";
+            this.buttonFind2Form.Size = new System.Drawing.Size(104, 23);
+            this.buttonFind2Form.TabIndex = 2;
+            this.buttonFind2Form.Text = "Найти";
+            this.buttonFind2Form.UseVisualStyleBackColor = true;
+            this.buttonFind2Form.Click += new System.EventHandler(this.buttonFind2Form_Click);
+            // 
+            // FindTextForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 92);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonFind2Form);
+            this.Controls.Add(this.FindTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "FindTextForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поиск слова";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FindTextBox;
+        private System.Windows.Forms.Button buttonFind2Form;
+    }
+}
